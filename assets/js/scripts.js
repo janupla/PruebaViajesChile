de314d010c5f1ceb44d9bd7187f303c9c6f80d33
Jana <janagonzalez.f@gmail.com>
2024-02-28 22:40:00 -0300

    agregando jquery al btn del form

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,2 +1,6 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+$("#boton").click(function(){
+    alert("Tu mensaje fue enviado exitosamente")
+})
